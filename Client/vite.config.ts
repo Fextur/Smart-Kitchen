5cd2529d54368554cf8c0a1922df567bc7381e4c
Cris <christina.modritski@gmail.com>
2025-03-19 13:00:03 +0200

    added pwa

--- Client/vite.config.ts
+++ Client/vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// https://vitejs.dev/config/
+export default defineConfig(() => {
+
+  return {
+    plugins: [
+      react(),
+      VitePWA({
+        registerType: 'autoUpdate',
+        devOptions: { enabled: true },
+        manifest: {
+          name: 'Smart Kitchen',
+          start_url: '/',
+          theme_color: '#ffffff',
+        },
+      }),
+    ],
+  };
+});
