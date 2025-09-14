ce8865b6831247a6d1fcc2d7ddb3cc61872096a3
Fextur <galavishay.ga@gmail.com>
2025-09-14 17:23:37 +0300

    removed loader for alerts, fixed bug that you cant remove items with amount zero from add products page, and on mobile now automatilcy tries to open file on image selection

--- Client/src/hooks/useLoading.tsx
+++ Client/src/hooks/useLoading.tsx
@@ -3,11 +3,19 @@ import { useIsFetching, useIsMutating } from "@tanstack/react-query";
 import { useAtomValue, useSetAtom } from "jotai";
 
 export const useLoading = () => {
-  const isFetching = useIsFetching();
-  const isMutating = useIsMutating();
+  const isFetching = useIsFetching({
+    predicate: (query) => {
+      if (query.queryKey[0] === "alerts") {
+        return false;
+      }
+      return true;
+    },
+  });
 
+  const isMutating = useIsMutating();
   const isManualLoading = useAtomValue(isLoadingAtom);
   const setLoading = useSetAtom(loadingCountAtom);
+
   return {
     isLoading: isFetching > 0 || isMutating > 0 || isManualLoading,
     setLoading: () => setLoading((prev) => prev + 1),
